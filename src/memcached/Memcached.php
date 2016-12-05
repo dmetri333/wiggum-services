9e5fadf8a953ae20af1c0c0bbbb14f373cb9f48b
Demetri <dmetri333@gmail.com>
2016-12-05 10:59:53 -0500

    added memcache

---
+++ src/memcached/Memcached.php
@@ -0,0 +1,36 @@
+<?php
+namespace app\services\memcached;
+
+class Memcached {
+	
+	protected $memcached;
+	
+	/**
+	 * 
+	 */
+	public function __construct($config) {
+		$this->memcached = new \Memcached();
+		$this->memcached->addServer($config['host'], $config['port']);
+	}
+	
+	/**
+	 * 
+	 * @param string $key
+	 * @param mixed $value
+	 * @param int $expiration
+	 * @return boolean
+	 */
+	public function set($key, $value, $expiration = 0) {
+		return $this->memcached->set($key, $value, $expiration);
+	}
+	
+	/**
+	 * 
+	 * @param string $key
+	 * @return mixed
+	 */
+	public function get($key) {
+		return $this->memcached->get($key);
+	}
+	
+}
