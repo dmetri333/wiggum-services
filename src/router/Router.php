70c17309a5dac00ce3c035ae53e813e76fa00d26
Demetri Mihalakakos <demetri@Demetris-MacBook-Pro-4.local>
2018-01-24 23:03:15 -0500

    router service now has more control over routing

--- src/router/Router.php
+++ src/router/Router.php
@@ -3,7 +3,6 @@ namespace wiggum\services\router;
 
 use \InvalidArgumentException;
 use \wiggum\http\Request;
-use \wiggum\http\Response;
 use \wiggum\exceptions\PageNotFoundException;
 use \wiggum\exceptions\InternalErrorException;
 use \FastRoute\Dispatcher;
@@ -11,38 +10,107 @@ use \FastRoute\RouteCollector;
 
 class Router extends \wiggum\foundation\Router {
     
+    protected $routeInfo = null;
     protected $routes = [];
     protected $routeGroups = [];
     protected $routeCounter;
-
+    protected $registeredMiddleware;
+    protected $registeredfilters;
+    
+    
+    /**
+     *
+     * @param string $name
+     * @param \Closure $closure
+     */
+    public function registerMiddleware($name, $closure) {
+        $this->registeredMiddleware[$name] = $closure;
+    }
+    
+    /**
+     *
+     * @param string $name
+     * @param \Closure $closure
+     */
+    public function registerFilter($name, $closure) {
+        $this->registeredfilters[$name] = $closure;
+    }
+    
+    /**
+     * 
+     * @param string $pattern
+     * @param mixed $handler
+     * @return \wiggum\services\router\Route
+     */
     public function get($pattern, $handler) {
         return $this->map(['GET'], $pattern, $handler);
     }
 
+    /**
+     *
+     * @param string $pattern
+     * @param mixed $handler
+     * @return \wiggum\services\router\Route
+     */
     public function post($pattern, $handler) {
         return $this->map(['POST'], $pattern, $handler);
     }
 
+    /**
+     *
+     * @param string $pattern
+     * @param mixed $handler
+     * @return \wiggum\services\router\Route
+     */
     public function put($pattern, $handler) {
         return $this->map(['PUT'], $pattern, $handler);
     }
 
+    /**
+     *
+     * @param string $pattern
+     * @param mixed $handler
+     * @return \wiggum\services\router\Route
+     */
     public function patch($pattern, $handler) {
         return $this->map(['PATCH'], $pattern, $handler);
     }
 
+    /**
+     *
+     * @param string $pattern
+     * @param mixed $handler
+     * @return \wiggum\services\router\Route
+     */
     public function delete($pattern, $handler) {
         return $this->map(['DELETE'], $pattern, $handler);
     }
 
+    /**
+     *
+     * @param string $pattern
+     * @param mixed $handler
+     * @return \wiggum\services\router\Route
+     */
     public function options($pattern, $handler) {
         return $this->map(['OPTIONS'], $pattern, $handler);
     }
 
+    /**
+     *
+     * @param string $pattern
+     * @param mixed $handler
+     * @return \wiggum\services\router\Route
+     */
     public function any($pattern, $handler) {
         return $this->map(['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'], $pattern, $handler);
     }
     
+    /**
+     * 
+     * {@inheritDoc}
+     * @see \wiggum\foundation\Router::map()
+     */
     public function map($methods, $pattern, $handler) {
         
         if (!is_string($pattern)) {
@@ -76,118 +144,16 @@ class Router extends \wiggum\foundation\Router {
         
         return $route;
     }
-  
-	/**
-	 * 
-	 * @param Request $request
-	 * @param Response $response
-	 * @throws PageNotFoundException
-	 */
-	public function process(Request $request, Response $response) {
-        $actions = null;
-        
-        $routeDefinitionCallback = function (RouteCollector $r) {
-            foreach ($this->routes as $route) {
-                $r->addRoute($route->getMethods(), $route->getPattern(), $route->getIdentifier());
-            }
-        };
-        
-        $dispatcher = \FastRoute\simpleDispatcher($routeDefinitionCallback);
-        
-        $routeInfo = $dispatcher->dispatch($request->getMethod(), $request->getContextPath());
-       
-        switch ($routeInfo[0]) {
-            case Dispatcher::NOT_FOUND:
-                // ... 404 Not Found
-                
-                throw new PageNotFoundException();
-                
-                break;
-            case Dispatcher::METHOD_NOT_ALLOWED:
-                // ... 405 Method Not Allowed
-               
-                $allowedMethods = $routeInfo[1];
-                throw new InternalErrorException('Method Not Allowed ['.implode(', ', $allowedMethods).' accepted], Ref: '.$request->getContextPath(), 405);
-               
-                break;
-            case Dispatcher::FOUND:
-                $identifier = $routeInfo[1];
-                $parameters = $routeInfo[2];
-                
-                $actions = $this->controllerActions($identifier, $parameters);
-                
-                break;
-        }
-    
-        return $actions;
-    }
-    
-	/**
-	 * 
-	 * @param array $actions
-	 * @param mixed $data
-	 * @param array $parameters
-	 * @return multitype:array | null
-	 */
-    private function controllerActions($identifier, array $parameters = []) {
-        $route = $this->routes[$identifier];
-        $handler = $route->getCallable();
-        
-        $actions = [];
-        
-        // add filters to actions
-        if (!empty($route->getFilters())) {
-            $actions['filters'] = $this->filterArray($this->registeredfilters, $route->getFilters());
-        }
-        
-        // add middleware to actions
-        if (!empty($route->getMiddleware())) {
-            $actions['middleware'] = $this->filterArray($this->registeredMiddleware, $route->getMiddleware());
-        }
-       
-		if (is_string($handler)) {
-		    $routeSegments = explode('@', $handler); 
-		    
-		    $actions['classPath'] = $routeSegments[0];
-		
-		    if (isset($routeSegments[1]) && $routeSegments[1] != '') {
-		        $actions['method'] = $routeSegments[1];
-			}
-			
-			if (!empty($parameters)) {
-				$actions['parameters'] = $parameters;
-			}
-		
-			return $actions;
-		} else if (is_array($handler)) {
-		    if (isset($handler['classPath'])) {
-		        $actions['classPath'] = $handler['classPath'];
-			}
-			
-			if (isset($handler['method'])) {
-			    $actions['method'] = $handler['method'];
-			}
-			
-			if (!empty($parameters)) {
-				$actions['parameters'] = $parameters;
-			}
-			
-			if (isset($handler['properties'])) {
-			    $actions['properties'] = $handler['properties'];
-			}
-			
-			return $actions;
-		} else if (is_callable($handler)) {
-		    return array_merge($actions, (array) call_user_func_array($handler, [$parameters])) ;
-		}
-		
-		return null;
-	}
-
 	
 	
 	/* Groups */
 	
+    /**
+     * 
+     * @param unknown $attributes
+     * @param unknown $callable
+     * @return \wiggum\services\router\RouteGroup
+     */
 	public function group($attributes, $callable) {
 	    $group = $this->pushGroup($attributes, $callable);
 	    $group($this);
@@ -195,6 +161,10 @@ class Router extends \wiggum\foundation\Router {
 	    return $group;
 	}
 	
+	/**
+	 * 
+	 * @return array[]|string[]
+	 */
 	protected function processGroups() {
 	    $filters = [];
 	    $middleware = [];
@@ -210,17 +180,146 @@ class Router extends \wiggum\foundation\Router {
 	    return ['pattern' => $pattern, 'filters' => $filters, 'middleware' => $middleware ];
 	}
 	
+	/**
+	 * 
+	 * @param unknown $attributes
+	 * @param unknown $callable
+	 * @return \wiggum\services\router\RouteGroup
+	 */
 	protected function pushGroup($attributes, $callable) {
 	    $group = new RouteGroup($attributes, $callable);
 	    array_push($this->routeGroups, $group);
 	    return $group;
 	}
 	
+	/**
+	 * 
+	 * @return boolean|\wiggum\services\router\RouteGroup
+	 */
 	protected function popGroup() {
 	    $group = array_pop($this->routeGroups);
 	    return $group instanceof RouteGroup ? $group : false;
 	}
 	
+	
+	/* runners */
+	
+	/**
+	 *
+	 * {@inheritDoc}
+	 * @see \wiggum\foundation\Router::dispatch()
+	 */
+	public function dispatch(Request $request) {
+	    $actions = null;
+	    
+	    $routeDefinitionCallback = function (RouteCollector $r) {
+	        foreach ($this->routes as $route) {
+	            $r->addRoute($route->getMethods(), $route->getPattern(), $route->getIdentifier());
+	        }
+	    };
+	    
+	    $dispatcher = \FastRoute\simpleDispatcher($routeDefinitionCallback);
+	    
+	    $routeInfo = $dispatcher->dispatch($request->getMethod(), $request->getContextPath());
+	    
+	    switch ($routeInfo[0]) {
+	        case Dispatcher::NOT_FOUND:
+	            // ... 404 Not Found
+	            
+	            throw new PageNotFoundException();
+	            
+	            break;
+	        case Dispatcher::METHOD_NOT_ALLOWED:
+	            // ... 405 Method Not Allowed
+	            
+	            $allowedMethods = $routeInfo[1];
+	            throw new InternalErrorException('Method Not Allowed ['.implode(', ', $allowedMethods).' accepted], Ref: '.$request->getContextPath(), 405);
+	            
+	            break;
+	        case Dispatcher::FOUND:
+	            //store route info
+	            $this->routeInfo = $routeInfo;
+	            
+	            // Attach additional route middleware to app
+	            $route = $this->routes[$routeInfo[1]];
+	            if (!empty($route->getMiddleware())) {
+	                $middlewares = $this->filterArray($this->registeredMiddleware, $route->getMiddleware());
+	                foreach ($middlewares as $middleware) {
+	                    $this->app->addMiddleware($middleware);
+	                }
+	            }
+	            
+	            return;
+	            
+	            break;
+	    }
+	    
+	    throw new PageNotFoundException();
+	}
+	
+	/**
+	 *
+	 * {@inheritDoc}
+	 * @see \wiggum\foundation\Router::process()
+	 */
+	public function process() {
+	    $identifier = $this->routeInfo[1];
+	    $parameters = $this->routeInfo[2];
+	    
+	    $route = $this->routes[$identifier];
+	    $handler = $route->getCallable();
+	    
+	    $actions = [];
+	    
+	    if (is_string($handler)) {
+	        $routeSegments = explode('@', $handler);
+	        
+	        $actions['classPath'] = $routeSegments[0];
+	        
+	        if (isset($routeSegments[1]) && $routeSegments[1] != '') {
+	            $actions['method'] = $routeSegments[1];
+	        }
+	        
+	        if (!empty($parameters)) {
+	            $actions['parameters'] = $parameters;
+	        }
+	        
+	        
+	    } else if (is_array($handler)) {
+	        if (isset($handler['classPath'])) {
+	            $actions['classPath'] = $handler['classPath'];
+	        }
+	        
+	        if (isset($handler['method'])) {
+	            $actions['method'] = $handler['method'];
+	        }
+	        
+	        if (!empty($parameters)) {
+	            $actions['parameters'] = $parameters;
+	        }
+	        
+	        if (isset($handler['properties'])) {
+	            $actions['properties'] = $handler['properties'];
+	        }
+	        
+	    } else if (is_callable($handler)) {
+	        $actions = (array) call_user_func_array($handler, [$parameters]);
+	    }
+	    
+	    //run filters
+	    if (!empty($route->getFilters())) {
+	        $filters = $this->filterArray($this->registeredfilters, $route->getFilters());
+	        foreach ($filters as $filter) {
+	            $actions = $filter($actions);
+	        }
+	    }
+	    
+	    return $actions;
+	}
+	
+	
+	
+	/* helpers */
 	
 	private function filterArray($array, $allowed) {
 	    return array_intersect_key($array, array_flip($allowed));
